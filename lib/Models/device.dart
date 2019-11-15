66a9b87923d7fb59e076b194105eb92f6923a406
mibkvr <40341638+mibkvr@users.noreply.github.com>
2019-11-15 15:02:26 +0100

    Upgrade Device #9

--- lib/Models/device.dart
+++ lib/Models/device.dart
@@ -2,13 +2,12 @@ import 'package:CrypticMobile/Websocket/Request.dart';
 import 'package:uuid/uuid.dart';
 
 class Device {
-
   var uuid;
   String name;
   var owner;
   bool powered;
 
-  Device(var uuid, String name, var owner, bool powered) {
+  Device({var uuid, String name, var owner, bool powered}) {
     this.uuid = uuid;
     this.name = name;
     this.owner = owner;
@@ -17,13 +16,19 @@ class Device {
 
   static Device getDeviceByUUID(var uuid) {
     Device device;
-    Request('{"ms":"device","endpoint":["device","info"],"data":{"device_uuid":"'
-        + uuid + '"},"tag":"' + Uuid().v4() + '"}')
+    Request('{"ms":"device","endpoint":["device","info"],"data":{"device_uuid":"' +
+            uuid +
+            '"},"tag":"' +
+            Uuid().v4() +
+            '"}')
         .subscribe((data) {
-          if (!data.containsKey('error')) {
-            device = Device(Uuid().parse(data['uuid']), data['name'],
-                Uuid().parse(data['owner']), data['powered_on']);
-          }
+      if (!data.containsKey('error')) {
+        device = Device(
+            name: data['name'],
+            uuid: Uuid().parse(data['uuid']),
+            owner: Uuid().parse(data['owner']),
+            powered: data['powered_on']);
+      }
     });
 
     return device;
