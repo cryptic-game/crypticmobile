a7b8e9978e533829b502885d756fa2cde3f2e67e
mibkvr <40341638+mibkvr@users.noreply.github.com>
2019-11-16 18:57:46 +0100

    Fixed Device Wrapper Class #9

--- lib/Models/device.dart
+++ lib/Models/device.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:CrypticMobile/Websocket/Request.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:uuid/uuid.dart';
 
 class Device {
@@ -7,14 +10,19 @@ class Device {
   var owner;
   bool powered;
 
-  Device({var uuid, String name, var owner, bool powered}) {
+  Device(
+      {@required var uuid,
+      @required String name,
+      @required var owner,
+      @required bool powered}) {
     this.uuid = uuid;
     this.name = name;
     this.owner = owner;
     this.powered = powered;
   }
 
-  static Device getDeviceByUUID(var uuid) {
+  static Future<dynamic> getDeviceByUUID(var uuid) {
+    Completer c = new Completer();
     Device device;
     Request('{"ms":"device","endpoint":["device","info"],"data":{"device_uuid":"' +
             uuid +
@@ -28,9 +36,37 @@ class Device {
             uuid: Uuid().parse(data['uuid']),
             owner: Uuid().parse(data['owner']),
             powered: data['powered_on']);
+
       }
+      c.complete(device);
     });
 
-    return device;
+    return c.future;
+  }
+
+  static Future<dynamic> getAllDevices() {
+    Completer c = new Completer();
+    List<Device> list = new List<Device>();
+
+    Request('{"ms":"device","endpoint":["device","all"],"data":{},"tag":"' +
+            Uuid().v4() +
+            '"}')
+        .subscribe((data) {
+      if (!data.containsKey('error')) {
+        for (var a in data['devices']) {
+          list.add(Device(
+              name: a['name'],
+              uuid: Uuid().parse(a['uuid']),
+              owner: Uuid().parse(a['owner']),
+              powered: a['powered_on']));
+        }
+      }
+      for (var i = 0; i < 5; i++) {
+        list.add(Device(
+            powered: true, name: "Big Berta", uuid: Uuid().v4(), owner: "Max"));
+      }
+      c.complete(list);
+    });
+    return c.future;
   }
 }
